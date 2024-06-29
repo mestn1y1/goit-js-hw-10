79b1712616753a6ce92187ff49a629cbf9a2e5f4
Serhii Tyron <mestn1y@hotmail.com>
2024-06-29 20:50:10 +0300

    hw10-2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(document.querySelector('.input-delay').value);
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  const delayPromise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+
+  delayPromise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+});
